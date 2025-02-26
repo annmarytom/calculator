f87fd3035c85acbce57ba028ff098f05b35f395b
annmarytom <annmarytom9@gmail.com>
2025-02-26 23:27:53 +0400

    second commit-calculator button creation and its functionalities

--- calculator.js
+++ calculator.js
@@ -1,14 +1,18 @@
 
-    document.getElementById("darkMode-toggle-1").addEventListener('click',function ()
-    {
-        var element= document.getElementsByClassName("main-div")[0];
-        console.log(element.style.backgroundColor);
-        if(element.style.backgroundColor == "black"){
 
-            element.style.backgroundColor = "white";
-            
-        }else{
-            element.style.backgroundColor = "black";
+document.getElementById("darkMode-toggle-1").addEventListener('click', function () {
+    var element = document.getElementsByClassName("main-div")[0];
 
-        }
-       }  ); 
+    if (element.style.backgroundColor == "black") {
+
+        element.style.backgroundColor = "white";
+
+    } else {
+        element.style.backgroundColor = "black";
+
+    }
+});
+function posNegConversion() {
+    if (resultArea.value > 0 || resultArea.value < 0 ? resultArea.value = resultArea.value * -1 : "") {
+    }
+}
